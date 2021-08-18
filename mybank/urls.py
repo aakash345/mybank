1b110850f58bbccc7cf3d151f150296b603496a3
Aakash Gupta <aakagupta375@gmail.com>
2021-08-19 01:18:48 +0530

    update

--- mybank/urls.py
+++ mybank/urls.py
@@ -20,7 +20,7 @@ from django.views.static import serve
 from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(r'^admin/', admin.site.urls),
     path('', include('accounts.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
